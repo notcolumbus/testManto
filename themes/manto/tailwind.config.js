e1469ddbf273a7d6d977bf0e072a1d878c5af26b
Aman Anwar <aman06@vt.edu>
2025-07-29 23:03:17 +0530

    initial commit

---
+++ themes/manto/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./layouts/**/*.html",
+    "../../layouts/**/*.html",
+    "../../content/**/*.md",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
