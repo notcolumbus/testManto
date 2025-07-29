56b248cc2be17b8aa188e1af646c4699099b0c94
Aman Anwar <aman06@vt.edu>
2025-07-29 23:23:50 +0530

    tailwind added

--- themes/manto/tailwind.config.js
+++ themes/manto/tailwind.config.js
@@ -2,8 +2,12 @@
 module.exports = {
   content: [
     "./layouts/**/*.html",
+    "./layouts/**/*.js",
     "../../layouts/**/*.html",
     "../../content/**/*.md",
+    "../../content/**/*.html",
+    "./content/**/*.md",
+    "./content/**/*.html",
   ],
   theme: {
     extend: {},
